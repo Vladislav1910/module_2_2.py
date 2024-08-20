5426b00be619b110b67e5195ee86c3816411f67a
Vladislav1910 <vlad.makarenk@yandex.ru>
2024-08-20 17:27:11 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,9 @@
+first = 19
+second = 10
+third = 98
+if first == second and second == third:
+    print(3)
+elif first == second or first == third or second == third:
+    print(2)
+else:
+    print(0)
